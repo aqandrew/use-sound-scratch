9b3e6253c52b9bea07cd8945f24f4fb1fd9be138
dawneraq <dawneraq@gmail.com>
2020-04-22 11:11:13 -0700

    move play functions into handleScroll, with horrible glitchy sounds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSound from 'use-sound';
 
 import scratchSfx from '../src/sounds/scratch.mp3';
 
 function App() {
+  const [lastKnownScrollTop, setLastKnownScrollTop] = useState(0);
   const [play] = useSound(scratchSfx, {
     sprite: {
       down: [66, 115],
@@ -11,10 +12,21 @@ function App() {
     },
   });
 
+  const handleScroll = (event) => {
+    let scrollTop = event.target.scrollTop;
+
+    if (scrollTop > lastKnownScrollTop) {
+      play({ id: 'up' });
+    } else {
+      play({ id: 'down' });
+    }
+
+    setLastKnownScrollTop(scrollTop <= 0 ? 0 : scrollTop);
+  };
+
   return (
-    <div className="App">
-      <button onClick={() => play({ id: 'down' })}>play down</button>
-      <button onClick={() => play({ id: 'up' })}>play up</button>
+    <div className="App" onScroll={handleScroll}>
+      <div className="inner"></div>
     </div>
   );
 }
