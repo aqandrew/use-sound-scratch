b2b6c23718f7f4e4b91226e3c97c4a4772446837
dawneraq <dawneraq@gmail.com>
2020-04-22 10:58:42 -0700

    split scratchSfx into up/down

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import useSound from 'use-sound';
 import scratchSfx from '../src/sounds/scratch.mp3';
 
 function App() {
-  const [play] = useSound(scratchSfx);
+  const [play] = useSound(scratchSfx, {
+    sprite: {
+      down: [66, 115],
+      up: [145, 160],
+    },
+  });
 
   return (
     <div className="App">
-      <button onClick={play}>play sound</button>
+      <button onClick={() => play({ id: 'down' })}>play down</button>
+      <button onClick={() => play({ id: 'up' })}>play up</button>
     </div>
   );
 }
