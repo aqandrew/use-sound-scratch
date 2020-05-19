cfac973b59ee8b3287a2cd88a51c1ac9a207b92c
dawneraq <dawneraq@gmail.com>
2020-05-18 19:12:29 -0700

    add button to click before playing sound

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useSound from 'use-sound';
 
 import scratchSfx from './sounds/scratch.mp3';
 
 function App() {
+  const [okayClicked, setOkayClicked] = useState(false);
   const [lastKnownScrollTop, setLastKnownScrollTop] = useState(0);
   const [isScrollingDown, setIsScrollingDown] = useState(false);
   const [play] = useSound(scratchSfx, {
@@ -13,8 +14,18 @@ function App() {
     },
   });
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
   const handleScroll = (event) => {
-    let scrollTop = event.target.scrollTop;
+    // TODO Attach handleScroll to .App in JSX below,
+    // after figuring out this Pen's CSS witchcraft
+    // https://codepen.io/geoffgraham/pen/KLGKqJ
+    const scrollingElement = document.querySelector('html');
+    let scrollTop = scrollingElement.scrollTop;
 
     if (scrollTop > lastKnownScrollTop) {
       if (!isScrollingDown) {
@@ -34,8 +45,13 @@ function App() {
   };
 
   return (
-    <div className="App" onScroll={handleScroll}>
-      <h1>Scroll me!</h1>
+    <div className="App">
+      <div id="instructions">
+        <h1>Scroll me!</h1>
+        {!okayClicked ? (
+          <button onClick={() => setOkayClicked(true)}>Okay</button>
+        ) : null}
+      </div>
       <div className="inner"></div>
     </div>
   );
